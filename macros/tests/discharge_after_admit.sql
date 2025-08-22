02ea2433957c3e932362df8a3e9e6f42fe092b6b
cfuglestad <92127306+cfuglestad@users.noreply.github.com>
2025-08-22 08:48:22 -0500

    Update discharge_after_admit.sql

--- macros/tests/discharge_after_admit.sql
+++ macros/tests/discharge_after_admit.sql
@@ -1,5 +1,8 @@
-{% test discharge_after_admit(model, admit_col, discharge_col) %}
-    select *
-    from {{ model }}
-    where {{ discharge_col }} < {{ admit_col }}
+{% test discharge_after_admit(model, column_name, admit_col, discharge_col) %}
+-- Fails if any discharge occurs before admit (or is null while admit is not)
+select *
+from {{ model }}
+where {{ discharge_col }} is null
+   or {{ admit_col }} is null
+   or {{ discharge_col }} < {{ admit_col }}
 {% endtest %}
